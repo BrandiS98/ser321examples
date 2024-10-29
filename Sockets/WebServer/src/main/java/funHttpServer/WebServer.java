dc6bbf9f2532bee4dc63713a4fa76a01d3a98f28
Brandi S <122651856+BrandiS98@users.noreply.github.com>
2024-10-28 19:05:36 -0500

    Update WebServer.java
    Updated catches

--- Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
+++ Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
@@ -223,7 +223,7 @@ class WebServer {
                  builder.append("Content-Type: text/html; charset=utf-8\n");
                   builder.append("\n");
                   builder.append("Missing data.");
-           }catch(NumberFormatException e){
+           }catch(InputMismatchException e){
                 builder.append("HTTP/1.1 406 Not Acceptable\n");
                 builder.append("Content-Type: text/html; charset=utf-8\n");
                 builder.append("\n");
@@ -250,6 +250,7 @@ class WebServer {
           builder.append("Check the todos mentioned in the Java source file");
           // TODO: Parse the JSON returned by your fetch and create an appropriate
           // response based on what the assignment document asks for
+         
            
        
            
